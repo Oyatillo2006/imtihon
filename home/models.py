70b6751efdaa58670dcd1879a03162372be900ac
Oyatillo <oyatilloabdullayev2006@gmail.com>
2023-09-18 16:45:02 -0700

    create model (Todo) and add to admin

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    STATUS = (
+        ("bajarildi", "Bajarildi"),
+        ("bajarilmadi", "Bajarilmadi"),
+        ("bajarilmoqda", "Bajarilmoqda")
+    )
+
+    title = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    docs = models.TextField()
+    status = models.CharField(max_length=15, choices=STATUS)
+
+    def __str__(self):
+        return self.title
