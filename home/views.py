9bdd93836def70d3e5ff145c06fae823a0349533
Oyatillo <oyatilloabdullayev2006@gmail.com>
2023-09-18 17:05:10 -0700

    connect add button

--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
-# Create your views here.
+def home(request):
+    if request.POST:
+        title = request.POST.get("title")
+        time = request.POST.get("time")
+        docs = request.POST.get("docs")
+        status = request.POST.get("status")
+
+        Todo.objects.create(title = title, time = time, docs = docs, status = status)
+        redirect("/")
+
+    context = {
+        "todos": Todo.objects.all()
+    }
+    return render(request, "todo.html", context)
+
