15fc9f93446ee6677fab06f94e884c5e0b0507d6
Oyatillo <oyatilloabdullayev2006@gmail.com>
2023-09-18 17:30:07 -0700

    add edit button

--- home/views.py
+++ home/views.py
@@ -21,3 +21,19 @@ def home(request):
 def delete(request, id):
     Todo.objects.get(id=id).delete()
     return redirect("/")
+
+def edit(request, id):
+    if request.POST:
+        title = request.POST.get("title")
+        time = request.POST.get("time")
+        docs = request.POST.get("docs")
+        status = request.POST.get("status")
+
+        Todo.objects.filter(id=id).update(title = title, time = time, docs = docs, status = status)
+        return redirect("/")
+
+    data = {
+        "todo": Todo.objects.get(id=id)
+    }
+
+    return render(request, "edit_todo.html", data)
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from home.views import home, delete
+from home.views import home, delete, edit
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home),
     path("delete/<int:id>/", delete),
+    path("edit/<int:id>/", edit),
 ]
